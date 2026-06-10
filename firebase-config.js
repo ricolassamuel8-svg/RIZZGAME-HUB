8ea5bf3b5c5b14e784457ac63cedd34a0361e265
ricolassamuel8-svg <ricolassamuel8@gmail.com>
2026-06-10 20:13:18 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDDOcUSo9SzM6rSKUO29Thw7Qke3I6SK9g",
+  authDomain: "rizzgame-hub.firebaseapp.com",
+  databaseURL: "https://rizzgame-hub-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "rizzgame-hub",
+  storageBucket: "rizzgame-hub.firebasestorage.app",
+  messagingSenderId: "1056785964458",
+  appId: "1:1056785964458:web:a22b151c45f8a21b965fd3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
